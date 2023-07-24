d82c10c63a7156053ce62cff4c6f509424d06d13
RavaT <7sravat@gmail.com>
2023-07-25 02:28:36 +0530

    search box typing text animation added

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -8,10 +8,51 @@ function openForm(formType) {
   }
 }
 function closeForm(formType) {
-    if (formType === "SignUp") {
-        document.getElementById("signup").classList.remove("active");
-      } else {
-       
-        document.getElementById("signin").classList.remove("active");
-      }
+  if (formType === "SignUp") {
+    document.getElementById("signup").classList.remove("active");
+  } else {
+    document.getElementById("signin").classList.remove("active");
+  }
 }
+
+// js for search input
+
+const searchInput = document.getElementById("search-input");
+const searchPlaceholder = document.getElementById("search-input-placeholder");
+const additionalText = [
+  "Search for  \"Vegetables\" ",
+  "Search for \"Fruits\"",
+  "Search for \"Herbs\"",
+  "Search for \"Spices\"",
+];
+let additionalTextIndex = 0;
+let typingAnimation;
+
+function animateText(text) {
+  searchPlaceholder.textContent = "";
+  let charIndex = 0;
+  typingAnimation = setInterval(() => {
+    if (charIndex <= text.length) {
+      searchPlaceholder.textContent = text.slice(0, charIndex);
+      charIndex++;
+    } else {
+      clearInterval(typingAnimation);
+      setTimeout(() => {
+        additionalTextIndex = (additionalTextIndex + 1) % additionalText.length;
+        animateText(additionalText[additionalTextIndex]);
+      }, 1000);
+    }
+  }, 100);
+}
+
+searchInput.addEventListener("focus", () => {
+  clearInterval(typingAnimation);
+  searchPlaceholder.textContent = "";
+});
+
+searchInput.addEventListener("blur", () => {
+  animateText(additionalText[additionalTextIndex]);
+  additionalTextIndex = (additionalTextIndex + 1) % additionalText.length;
+});
+
+animateText(additionalText[additionalTextIndex]);
